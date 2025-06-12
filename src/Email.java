87173064aa0960688f7c60b2523042348b769889
leila <leila.m2006131@gmail.com>
2025-06-13 00:47:31 +0330

    add Email method

--- src/Email.java
+++ src/Email.java
@@ -1,2 +1,47 @@
+import java.util.List;
+
 public class Email {
+    private String code;
+    private String sender;
+    private List<String> recipients;
+    private String subject;
+    private String body;
+    private boolean read;
+
+    public Email(String code, String sender, List<String> recipients, String subject, String body) {
+        this.code = code;
+        this.sender = sender;
+        this.recipients = recipients;
+        this.subject = subject;
+        this.body = body;
+        this.read = false;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public List<String> getRecipients() {
+        return recipients;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
 }
