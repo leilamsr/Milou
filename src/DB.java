f54705e3dfd56b7c32bd59220b49f1654c958c72
leila <leila.m2006131@gmail.com>
2025-06-13 00:07:23 +0330

    add db method

--- src/DB.java
+++ src/DB.java
@@ -1,2 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DB {
+    private static final String url = "jdbc:postgresql://vinson.liara.cloud:33839/nice_wing";
+    private static final String user = "root";
+    private static final String password = "4qQixj5bMn4DGH7nGEjUlSli";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
